49af7444d7799fda4109bbace8601353ebc4b02e
nouirasahar <nouirasahar91@gmail.com>
2025-03-28 00:47:57 +0100

    .

---
+++ OurWebsite/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'OurWebsite';
+}
---
+++ OurWebsite/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route to the HomeComponent
+  { path: '', component: NavbarComponent } 
+];
+
+
+
